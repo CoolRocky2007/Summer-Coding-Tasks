00c3014168c328cd030dc40f243d7b17dd41a3f6
CoolRocky2007 <90528490+CoolRocky2007@users.noreply.github.com>
2025-06-20 22:04:10 +0100

    Add files via upload

--- PCTC_Warm-Up_1.py
+++ PCTC_Warm-Up_1.py
@@ -1,10 +1,10 @@
 #19.06.25
-amount_list = []
+amount_list = []        #initialising list
 total = 0
-for i in range(5):
+for i in range(5):      #iterating 5 times
     amount = int(input())
-    amount_list.append(amount)
-    total += amount
+    amount_list.append(amount)      #appending amount to list
+    total += amount     #calculating total amount
 index = int(input())
-print('$' + str(total - amount_list[index-1]))
+print('$' + str(total - amount_list[index-1]))      #printing the amount left after subtracting amount given from index
 #3/3 Full Score
