809f9d2b41cdb116a34d68715e5791cda2d8286b
CoolRocky2007 <90528490+CoolRocky2007@users.noreply.github.com>
2025-06-23 23:30:38 +0100

    Delete PCTC_Warm-Up_5.py

--- PCTC_Warm-Up_5.py
+++
@@ -1,9 +0,0 @@
-#20.06.25
-total = int(input())        #ensuring total != 0, and if it is, output = 1
-count = 1
-while total != 0:       #while loop checking the condition
-    num = int(input())
-    total+=num      #total calculation
-    count+=1
-print(count)        #printing the amount of numbers inputted
-#3/3 Full Score
