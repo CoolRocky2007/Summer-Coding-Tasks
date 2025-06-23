0e29b4d59ccbc5491733ae72bc2def0ad77d8459
CoolRocky2007 <90528490+CoolRocky2007@users.noreply.github.com>
2025-06-23 23:30:16 +0100

    Delete PCTC_Warm-Up_3.py

--- PCTC_Warm-Up_3.py
+++
@@ -1,9 +0,0 @@
-#19.06.25
-num = int(input())
-if num%8 == 0:      #if num is a multiple of 8 already
-    print(num)
-elif num%8 <= 4:        #if remainder of num/8 is smaller than or equal to four
-    print(num-(num%8))      #finding the closest multiple of 8 lower than num
-else:
-    print(num+(8-(num%8)))      #finding the closest multiple of 8 bigger than num
-#2/3 Partial Score
