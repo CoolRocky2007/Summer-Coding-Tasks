962f85ae9bda90af45c9baec1d2b35adfd3495d9
CoolRocky2007 <90528490+CoolRocky2007@users.noreply.github.com>
2025-06-23 23:35:54 +0100

    Create PCTC_Sample_R2_Q1.py

---
+++ PCTC Sample R2/PCTC_Sample_R2_Q1.py
@@ -0,0 +1,13 @@
+#23.06.25
+x = float(input())
+y = float(input())
+z = float(input())
+
+if x < y < z:
+    print("UP")
+elif x > y > z:
+    print("DOWN")
+else:
+    print("WOBBLY")
+    
+#10/10 Full Score
